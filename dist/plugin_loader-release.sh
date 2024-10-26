3af8d8e2b3bb11dfaa9f9d501cd4dbddfdc1419f
Jared Henderson <34703139+jaredhenderson@users.noreply.github.com>
2024-10-26 17:23:40 +1100

    Customisations for Batocera 40 install

---
+++ dist/plugin_loader-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+logfile=/tmp/decky-loader-log.txt
+
+USER_DIR="/userdata/saves/steam"
+HOMEBREW_FOLDER="${USER_DIR}/homebrew"
+     
+case "$1" in
+    start)
+        echo "Starting Decky Loader"
+        export UNPRIVILEGED_PATH=${HOMEBREW_FOLDER}
+        export PRIVILEGED_PATH=${HOMEBREW_FOLDER}
+        export LOG_LEVEL=INFO
+        cd ${HOMEBREW_FOLDER}/services
+        PluginLoader
+        ;;
+    stop)
+        echo "Stopping Decky Loader"
+        ;;
+    status)
+        echo "Decky Loader status"
+        ;;
+esac
